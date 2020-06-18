f5f7bc9e8531bee980cb6cafeb3daf6cfca5778e
Clarissa Velásquez <43384904+clarissavlsqz@users.noreply.github.com>
2020-06-18 00:58:02 -0500

    Update Level1.py

--- Level1.py
+++ Level1.py
@@ -1,12 +1,16 @@
+# Write a function that capitalizes the first and fourth letters of a name
 def old_macdonald(name):
     first = name[:3]
     second = name[3:]
     return first.capitalize() + second.capitalize()
-    
+
+# Given a sentence, return a sentence with the words reversed
 def master_yoda(sentence):
     new = sentence.split()
     new.reverse()
     return " ".join(new)
-    
+
+
+# Given an integer n, return True if n is within 10 of either 100 or 200    
 def almost_there(n):
     return abs(100-n) <= 10 or abs(200-n) <= 10    
