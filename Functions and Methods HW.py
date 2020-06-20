0fec30690b42ab3be3cdb3ef700cd629ec014dd8
Clarissa Velásquez <43384904+clarissavlsqz@users.noreply.github.com>
2020-06-20 00:10:30 -0500

    Update Functions and Methods HW.py

--- Functions and Methods HW.py
+++ Functions and Methods HW.py
@@ -26,3 +26,7 @@ def up_low(s):
     print(f"No. of Lower case chars: {contLo}")
     
     
+# Write a Python function that takes a list and returns a new list with unique elements of the first list.
+def unique_list(lst):
+    return list(set(lst))
+    
